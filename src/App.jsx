a70e485b8f1bbf80db73bd36efe743f7ca4c41f4
Archanach707 <archanach707@gmail.com>
2024-08-17 14:48:32 +0530

    all changes added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { Button, Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
 

d524aada3e6bc7ff18b110107242fd42865b7b83
Archanach707 <archanach707@gmail.com>
2024-08-30 21:42:49 +0530

    navigation section added sub menus

--- src/component/Nevigation/index.jsx
+++ src/component/Nevigation/index.jsx
@@ -11,26 +11,40 @@ const Nevigation = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mx-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Products</Nav.Link>
-                <Nav.Link href="#link">Services</Nav.Link>
-                <Nav.Link href="#link">Courses</Nav.Link>
-                <Nav.Link href="#link">Blogs</Nav.Link>
-                <Nav.Link href="#link">Videos</Nav.Link>
-                <Nav.Link href="#link">Contact Us</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
+                <Nav.Link href="#action1" active>
+                  Home
+                </Nav.Link>
+                <NavDropdown title="Products" id="basic-nav-dropdown">
+                  <NavDropdown
+                    title="Dhoop Cones"
+                    id="basic-nav-dropdown2"
+                    className="dropend"
+                  >
+                    <NavDropdown.Item href="#action/2.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/2.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/2.3">
+                      Something
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown.Item href="#action/1.1">
+                    Action1
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
+                  <NavDropdown.Item href="#action/1.2">
+                    Action2
                   </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
+                  <NavDropdown.Item href="#action/1.3">
+                    Action3
                   </NavDropdown.Item>
                 </NavDropdown>
+                <Nav.Link href="#3">Services</Nav.Link>
+                <Nav.Link href="#4">Courses</Nav.Link>
+                <Nav.Link href="#5">Blogs</Nav.Link>
+                <Nav.Link href="#6">Videos</Nav.Link>
+                <Nav.Link href="#7">Contact Us</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </div>
